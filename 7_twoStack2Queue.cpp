cd6b7961464edd1f0c6f3581a5849d9f9eca9c0e
Qzhenlong <qzl1990@gmail.com>
2015-06-06 09:00:14 +0800

    code

---
+++ 7_twoStack2Queue.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+/*
+题目：用两个栈实现一个队列。实现在队列尾部插入结点和队列头部删除结点的功能。
+思路：
+s2负责输出队首，不为空即可输出,为空则将s1所有元素导入s2
+s1负责入队
+*/
+class Solution{
+public:
+	Solution();
+	~Solution();
+	void appendTail(int e)
+	{
+		s1.push(e);
+	}
+	bool deleteHead(int &e)  
+	{
+		if(!s2.empty())
+		{
+			e = s2.top();
+			s2.pop();
+			return true;
+		}
+		else
+		{
+			while(!s1.empty())
+			{
+				s2.push(s1.top());
+				s1.pop();
+			}
+			if(!s2.empty())
+			{
+				e = s2.top();
+				s2.pop();
+				return true;
+			}
+		}
+		return false;
+	}
+private:
+	stack<int > s1;
+	stack<int > s2; 
+};
+
+int main()
+{
+	return 0;
+}
